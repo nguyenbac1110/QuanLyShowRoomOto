49c607ee9687d05a7e4b31d1bb116fd24a91de66
Vuong Quynh <vuongquynhhda@gmail.com>
2024-10-31 00:00:04 +0700

    CRUD Manufactory

--- QuanLyShowRoomOto/frmManufactory.Designer.cs
+++ QuanLyShowRoomOto/frmManufactory.Designer.cs
@@ -28,12 +28,346 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnluu = new System.Windows.Forms.Button();
+            this.pnlluu_boqua = new System.Windows.Forms.Panel();
+            this.btnboqua = new System.Windows.Forms.Button();
+            this.pnlthem_sua = new System.Windows.Forms.Panel();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.ManufactoryID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ManufactoryName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AddInfor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.txtTTBS = new System.Windows.Forms.TextBox();
+            this.txtMa = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.pnlluu_boqua.SuspendLayout();
+            this.pnlthem_sua.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnluu
+            // 
+            this.btnluu.Location = new System.Drawing.Point(25, 31);
+            this.btnluu.Name = "btnluu";
+            this.btnluu.Size = new System.Drawing.Size(75, 28);
+            this.btnluu.TabIndex = 100;
+            this.btnluu.Text = "Lưu";
+            this.btnluu.UseVisualStyleBackColor = true;
+            this.btnluu.Click += new System.EventHandler(this.btnluu_Click);
+            // 
+            // pnlluu_boqua
+            // 
+            this.pnlluu_boqua.Controls.Add(this.btnboqua);
+            this.pnlluu_boqua.Controls.Add(this.btnluu);
+            this.pnlluu_boqua.Location = new System.Drawing.Point(541, 270);
+            this.pnlluu_boqua.Name = "pnlluu_boqua";
+            this.pnlluu_boqua.Size = new System.Drawing.Size(253, 96);
+            this.pnlluu_boqua.TabIndex = 121;
+            this.pnlluu_boqua.Visible = false;
+            // 
+            // btnboqua
+            // 
+            this.btnboqua.Location = new System.Drawing.Point(158, 31);
+            this.btnboqua.Name = "btnboqua";
+            this.btnboqua.Size = new System.Drawing.Size(75, 28);
+            this.btnboqua.TabIndex = 101;
+            this.btnboqua.Text = "Bỏ qua";
+            this.btnboqua.UseVisualStyleBackColor = true;
+            this.btnboqua.Click += new System.EventHandler(this.btnboqua_Click);
+            // 
+            // pnlthem_sua
+            // 
+            this.pnlthem_sua.Controls.Add(this.btnthem);
+            this.pnlthem_sua.Controls.Add(this.btnxoa);
+            this.pnlthem_sua.Location = new System.Drawing.Point(541, 153);
+            this.pnlthem_sua.Name = "pnlthem_sua";
+            this.pnlthem_sua.Size = new System.Drawing.Size(253, 82);
+            this.pnlthem_sua.TabIndex = 120;
+            // 
+            // btnthem
+            // 
+            this.btnthem.Location = new System.Drawing.Point(16, 26);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(75, 28);
+            this.btnthem.TabIndex = 98;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(158, 26);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(75, 28);
+            this.btnxoa.TabIndex = 99;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.Location = new System.Drawing.Point(832, 180);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(75, 28);
+            this.btnsua.TabIndex = 119;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Times New Roman", 10.2F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ManufactoryID,
+            this.ManufactoryName,
+            this.Address,
+            this.Phone,
+            this.AddInfor});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Times New Roman", 10.2F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgv.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv.Location = new System.Drawing.Point(26, 419);
+            this.dgv.Name = "dgv";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Times New Roman", 10.2F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(950, 272);
+            this.dgv.TabIndex = 118;
+            this.dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellClick);
+            // 
+            // ManufactoryID
+            // 
+            this.ManufactoryID.DataPropertyName = "ManufactoryID";
+            this.ManufactoryID.HeaderText = "Mã nhà sản xuất";
+            this.ManufactoryID.MinimumWidth = 6;
+            this.ManufactoryID.Name = "ManufactoryID";
+            this.ManufactoryID.Width = 180;
+            // 
+            // ManufactoryName
+            // 
+            this.ManufactoryName.DataPropertyName = "ManufactoryName";
+            this.ManufactoryName.HeaderText = "Tên nhà sản xuất";
+            this.ManufactoryName.MinimumWidth = 6;
+            this.ManufactoryName.Name = "ManufactoryName";
+            this.ManufactoryName.Width = 180;
+            // 
+            // Address
+            // 
+            this.Address.DataPropertyName = "Address";
+            this.Address.HeaderText = "Địa chỉ";
+            this.Address.MinimumWidth = 6;
+            this.Address.Name = "Address";
+            this.Address.Width = 180;
+            // 
+            // Phone
+            // 
+            this.Phone.DataPropertyName = "Phone";
+            this.Phone.HeaderText = "Số điện thoại";
+            this.Phone.MinimumWidth = 6;
+            this.Phone.Name = "Phone";
+            this.Phone.Width = 180;
+            // 
+            // AddInfor
+            // 
+            this.AddInfor.DataPropertyName = "AddInfor";
+            this.AddInfor.HeaderText = "Thông tin bổ sung";
+            this.AddInfor.MinimumWidth = 6;
+            this.AddInfor.Name = "AddInfor";
+            this.AddInfor.Width = 180;
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(260, 285);
+            this.txtSDT.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(241, 22);
+            this.txtSDT.TabIndex = 116;
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(260, 250);
+            this.txtDiaChi.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(241, 22);
+            this.txtDiaChi.TabIndex = 115;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(100, 285);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(115, 23);
+            this.label10.TabIndex = 114;
+            this.label10.Text = "Số điện thoại:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(100, 250);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 23);
+            this.label9.TabIndex = 113;
+            this.label9.Text = "Địa chỉ:";
+            // 
+            // txtTen
+            // 
+            this.txtTen.Location = new System.Drawing.Point(260, 215);
+            this.txtTen.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(241, 22);
+            this.txtTen.TabIndex = 112;
+            // 
+            // txtTTBS
+            // 
+            this.txtTTBS.Location = new System.Drawing.Point(260, 320);
+            this.txtTTBS.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtTTBS.Name = "txtTTBS";
+            this.txtTTBS.Size = new System.Drawing.Size(241, 22);
+            this.txtTTBS.TabIndex = 110;
+            // 
+            // txtMa
+            // 
+            this.txtMa.Location = new System.Drawing.Point(260, 180);
+            this.txtMa.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtMa.Name = "txtMa";
+            this.txtMa.Size = new System.Drawing.Size(241, 22);
+            this.txtMa.TabIndex = 109;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(100, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(143, 23);
+            this.label7.TabIndex = 108;
+            this.label7.Text = "Tên nhà sản xuất:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(100, 320);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(155, 23);
+            this.label5.TabIndex = 106;
+            this.label5.Text = "Thông tin bổ sung:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(100, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 23);
+            this.label2.TabIndex = 104;
+            this.label2.Text = "Mã nhà sản xuất:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(42, 45);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(269, 45);
+            this.label11.TabIndex = 103;
+            this.label11.Text = "MANUFACTORY";
+            // 
+            // frmManufactory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(999, 733);
+            this.Controls.Add(this.pnlluu_boqua);
+            this.Controls.Add(this.pnlthem_sua);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.txtTTBS);
+            this.Controls.Add(this.txtMa);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label11);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmManufactory";
             this.Text = "frmManufactory";
+            this.Load += new System.EventHandler(this.frmManufactory_Load);
+            this.pnlluu_boqua.ResumeLayout(false);
+            this.pnlthem_sua.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnluu;
+        private System.Windows.Forms.Panel pnlluu_boqua;
+        private System.Windows.Forms.Button btnboqua;
+        private System.Windows.Forms.Panel pnlthem_sua;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.TextBox txtTTBS;
+        private System.Windows.Forms.TextBox txtMa;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ManufactoryID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ManufactoryName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Phone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AddInfor;
     }
 }
--- QuanLyShowRoomOto/frmManufactory.cs
+++ QuanLyShowRoomOto/frmManufactory.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,116 @@ namespace QuanLyShowRoomOto
 {
     public partial class frmManufactory : Form
     {
+        SqlConnection con;
+        SqlDataAdapter adapter = new SqlDataAdapter();
+
         public frmManufactory()
         {
             InitializeComponent();
         }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            string sqlINSERT = "INSERT INTO Manufactory( ManufactoryID, ManufactoryName, Address, Phone, AddInfor) VALUES ( @ManufactoryID, @ManufactoryName, @Address, @Phone, @AddInfor)";
+            SqlCommand cmd = new SqlCommand(sqlINSERT, con);
+            cmd.Parameters.AddWithValue("ManufactoryID", txtMa.Text);
+            cmd.Parameters.AddWithValue("ManufactoryName", txtTen.Text);
+            cmd.Parameters.AddWithValue("Address", txtDiaChi.Text);
+            cmd.Parameters.AddWithValue("Phone", txtSDT.Text);
+            cmd.Parameters.AddWithValue("AddInfor", txtTTBS.Text);
+            cmd.ExecuteNonQuery();
+            Hienthi();
+            Clear();
+            MessageBox.Show("Đã thêm mới xe thành công!", "Thông báo", MessageBoxButtons.OK); ;
+        }
+
+        private void frmManufactory_Load(object sender, EventArgs e)
+        {
+            string constring = ConfigurationManager.ConnectionStrings["ql"].ConnectionString.ToString();
+            con = new SqlConnection(constring);
+            con.Open();
+            Hienthi();
+        }
+
+        public void Hienthi()
+        {
+            string sqlSelect = "select * from Manufactory";
+            SqlCommand cmd = new SqlCommand(sqlSelect, con);
+            DataTable dt = new DataTable();
+            adapter.SelectCommand = cmd;
+            dt.Clear();
+            adapter.Fill(dt);
+            dgv.DataSource = dt;
+            dgv.AutoResizeColumns();
+        }
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            string sqlDELETE = "DELETE FROM Manufactory WHERE ManufactoryID = @ManufactoryID";
+            SqlCommand cmd = new SqlCommand(sqlDELETE, con);
+            cmd.Parameters.AddWithValue("@ManufactoryID", txtMa.Text);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                Hienthi();
+                Clear();
+                MessageBox.Show("Xóa thành công!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi xóa mặt hàng: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            Hienthi();
+        }
+
+        private void btnluu_Click(object sender, EventArgs e)
+        {
+           
+            string sqlEDIT = "UPDATE Manufactory SET ManufactoryName = @ManufactoryName, Address = @Address,Phone =@Phone, AddInfor = @AddInfor where ManufactoryID =@ManufactoryID";
+            SqlCommand cmd = new SqlCommand(sqlEDIT, con);
+            cmd.Parameters.AddWithValue("ManufactoryID", txtMa.Text);
+            cmd.Parameters.AddWithValue("ManufactoryName", txtTen.Text);
+            cmd.Parameters.AddWithValue("Address", txtDiaChi.Text);
+            cmd.Parameters.AddWithValue("Phone", txtSDT.Text);
+            cmd.Parameters.AddWithValue("AddInfor", txtTTBS.Text);
+            cmd.ExecuteNonQuery();
+            Hienthi();
+            MessageBox.Show("Đã sửa thông tin xe có mã xe là: " + txtMa.Text, "Thông báo", MessageBoxButtons.OK);
+        }
+
+        private void btnboqua_Click(object sender, EventArgs e)
+        {
+            pnlluu_boqua.Visible = false;
+            pnlthem_sua.Visible = true;
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            pnlluu_boqua.Visible = true;
+            pnlthem_sua.Visible = false;
+        }
+
+        private void dgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            if (e.RowIndex >= 0)
+            {
+                i = e.RowIndex;
+                txtMa.Text = dgv.Rows[i].Cells[0].Value.ToString();
+                txtTen.Text = dgv.Rows[i].Cells[1].Value.ToString();
+                txtDiaChi.Text = dgv.Rows[i].Cells[2].Value.ToString();
+                txtSDT.Text = dgv.Rows[i].Cells[3].Value.ToString();
+                txtTTBS.Text = dgv.Rows[i].Cells[4].Value.ToString();
+            }
+        }
+        public void Clear()
+        {
+            txtMa.Text = "";
+            txtTen.Text = "";
+            txtDiaChi.Text = "";
+            txtSDT.Text = "";
+            txtTTBS.Text = "";
+        }
+
     }
 }
